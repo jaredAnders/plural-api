6eff82b72e662374f1dd3fa71e720fde0e0f0a37
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-27 10:43:45 -0500

    adds update method to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,16 @@ class QuestionsController < ApplicationController
     render json: Question.all
   end
 
-  
+  def update
+    question = Question.find(params[:id])
+    question.update_attributes(question_params)
+    render json: question
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question_text, :answer)
+  end
+
 end
