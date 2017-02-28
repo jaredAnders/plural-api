db3c3e57136807f405254d055b22ee097b7537c3
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-28 17:26:24 -0500

    adds question list and question list item components

---
+++ client/app/bundles/Main/components/_question_list.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import QuestionListItem from './_question_list_item'
+
+export default class QuestionList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  handleUpdate(question) {
+    this.props.onUpdate(question);
+  }
+
+  render() {
+    const questions = this.props.questions.map((question) => {
+      return (
+        <QuestionListItem
+          key={question.id}
+          question={question}
+          handleUpdate={this.handleUpdate} />
+      )
+    })
+
+    return (
+      <ul>
+        {questions}
+      </ul>
+    )
+  }
+}
