33dabba31146e5eae7ea54869281689701c80ebe
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-28 17:25:37 -0500

    adds main component, removes old components

--- client/app/bundles/Main/startup/registration.jsx
+++ client/app/bundles/Main/startup/registration.jsx
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import Questions from '../components/Questions';
+import Main from '../components/Main';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  Questions,
+  Main,
 });
