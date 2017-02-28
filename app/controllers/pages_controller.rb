a22009bd4842112a1c32eac2d27bf13d69ef2cd8
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-28 17:18:47 -0500

    cleans pages controller and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,4 @@
 class PagesController < ApplicationController
   def index
-    question = Question.last
-    @questions_props = {
-      question: question['question_text'],
-      answer: question['answer']}
-  end
-
-  def import
-    Question.import
   end
 end
